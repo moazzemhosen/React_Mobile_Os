7f9e5e780cfac74495aff9a50319755c8e2ff70e
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-04-22 13:31:48 +0530

    done
    done

---
+++ src/components/Manufecture.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Problem11() {
+    return (
+        <>
+            <h1>Mobile Operating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>Iphone</li>
+                <li>Windows Phone</li>
+            </ul>
+        </>
+    )
+}
+function Problem12() {
+    return (
+        <>
+            <h1>Mobile Manufacturers</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>Htc</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+        </>
+    )
+}
+
+
+export {Problem11,Problem12};
